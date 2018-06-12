958d2550ea18f4336c8f336f5622b280a14eae01
MollyLJohnson <mollyl.johnson@att.net>
2018-06-12 17:56:24 -0500

    fixed navbar css

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,13 +4,18 @@ import '../styles/Navbar.css';
 export default class Navbar extends Component { 
   render() {
     return (
-        <nav className="navbar navbar-expand navbar-light bg-light sticky-top">
-            <div className="nav-item brand col-4">
-              <a href="/">Clicky Game</a>
-            </div>
-            <div className="nav-item col-4">Click an image to begin!</div>
-            <div className="nav-item col-4">Current Score: { this.props.score } | High Score: { this.props.highScore } </div>
-        </nav>
+      <nav className="navbar">
+        <div className="nav-item">
+          <a href="/">
+          Clicky Game
+          </a>
+        </div>
+        <div className="nav-item"></div>
+        <div className="nav-item">
+          Current Score: { this.props.score } | 
+          High Score: { this.props.highScore } 
+        </div>
+      </nav>
     )
   }
 }
