022033c3a7b454b417e090b0c4cedfe560c1ea02
MollyLJohnson <mollyl.johnson@att.net>
2018-06-12 18:02:05 -0500

    fixed the header css

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,16 +7,16 @@ export default class Header extends Component {
   render() {
     return (
       <header className = "header">
-      <Navbar 
-        score = { this.props.score }
-        highScore = { this.props.highScore }
-      />
-      <div className="jumbotron jumbotron-fluid">
-        <div className="container header text-center">
-          <h1 className="display-4">Clicky Game!</h1>
-          <h2 className="lead">Click on an image to earn points, but don't click on any more than once!</h2>
+        <Navbar 
+          score = { this.props.score }
+          highScore = { this.props.highScore }
+        />
+        <div className="jumbotron">
+          <div className="container">
+            <h1 className="">Orange Clicky Game!</h1>
+            <h2 className="lead">Click on an image to earn points, but don't click on any more than once!</h2>
+          </div>
         </div>
-      </div>
       </header>
     )
   }
