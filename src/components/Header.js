77cb8310c0f519537c82afbc8f9cb9e23a1b2c0e
MollyLJohnson <mollyl.johnson@att.net>
2018-06-12 19:13:35 -0500

    fixed all the css and functionality

--- src/components/Header.js
+++ src/components/Header.js
@@ -13,7 +13,7 @@ export default class Header extends Component {
         />
         <div className="jumbotron">
           <div className="container">
-            <h1 className="">Orange Clicky Game!</h1>
+            <h1 className="">Color Clicky Game!</h1>
             <h2 className="lead">Click on an image to earn points, but don't click on any more than once!</h2>
           </div>
         </div>
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import Orange from './Orange';
-import '../styles/Main.css';
 
 const src = "/orange.png";
 
